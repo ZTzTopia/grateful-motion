2cbfd5426ba896b5ce2481c52a7139932a61f39b
ztz <zentay36@gmail.com>
2026-01-27 21:30:25 +0700

    Initial Commit

---
+++ grateful-motionTests/grateful_motionTests.swift
@@ -0,0 +1,17 @@
+//
+//  grateful_motionTests.swift
+//  grateful-motionTests
+//
+//  Created by Muhamad Zibrisky on 27/01/26.
+//
+
+import Testing
+@testable import grateful_motion
+
+struct grateful_motionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
